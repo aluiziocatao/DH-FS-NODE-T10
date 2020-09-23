6d64d4fc9a7762bd7ef2c7f97e7fefbeea330afb
Aluízio Catão <aluizio2210@gmail.com>
2020-09-22 19:52:24 -0500

    Adicionando restante da aula10

--- aula10/index.js
+++ aula10/index.js
@@ -4,27 +4,11 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-//Servidor utilizando Express
-app.get('/', (req, res) => {
-  let array = ['item 1', 'item 2', 'item 3']
-  let arrayDeObjetos = [
-    {
-      "nome": "Aluízio"
-    },
-    {
-      "nome": "José"
-    }
-  ]
-  res.send(arrayDeObjetos)
-})
+// importando rotas da aplicacao
+const routes = require('./routes')
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`O Servidor está rodando na porta ${port}`)
 })
-
-//Node--> Possibilitar da gente conseguir trabalhar com JS no lado
-//do servidor mais conhecido como backend, ou seja, 
-//trabalhar com JS no backend
-
-//Express --> é um módulo que implementa diversas bibliotecas
-//ou pacotes para funcionar.
