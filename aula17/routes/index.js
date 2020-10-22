a18e10b16020fa82ed4b41717af87a6584b93473
Aluízio <aluizio2210@gmail.com>
2020-10-22 17:47:40 -0300

    Adicionando aula17

---
+++ aula17/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let cards = [
+    {
+      'caminho_imagem': './images/img01.jpg',
+      'titulo': 'Primeiro Título',
+      'texto': 'Primeiro texto do card que temos para exibir',
+      'botao': 'Ver mais'
+    },
+    {
+      'caminho_imagem': './images/img02.jpg',
+      'titulo': 'Segundo Título',
+      'texto': 'Segundo texto do card que temos para exibir',
+      'botao': 'Ver mais'
+    },
+    {
+      'caminho_imagem': './images/img03.png',
+      'titulo': 'Terceiro Título',
+      'texto': 'Terceiro texto do card que temos para exibir',
+      'botao': 'Ver mais'
+    },
+  ];
+
+  res.render('index', { cards: cards });
+});
+
+module.exports = router;
