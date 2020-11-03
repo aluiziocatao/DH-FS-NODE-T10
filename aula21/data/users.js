4a2e5e60e52416468008540303e76fbb6f417c02
Aluízio <aluizio2210@gmail.com>
2020-11-03 19:55:38 -0300

    Adicionando a aula 21

---
+++ aula21/data/users.js
@@ -0,0 +1,1 @@
+module.exports = [{"id":1,"name":"Admin","email":"admin@admin.com.br","password":"123456"},{"id":2,"name":"Viteira","email":"viteira@gmail.com","password":"123456"},{"id":3,"name":"Aluizio","email":"aneto@gmail.com","password":"$2b$10$EiYHdTAXd9Wayx7dKw6hIOPjcxWpygFzJJRgxHi73k8G./Yeab0QG"}]
---
+++ aula21/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const UserController = require("../controllers/UserController");
+
+router.get('/registrar', UserController.create);
+
+router.post('/registrar', UserController.save);
+
+router.get('/autenticar', UserController.login);
+
+
+module.exports = router;
